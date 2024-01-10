be98cc4f0ed0beb9944c5e01478923c7200f08de
Nancy <njerin016@gmail.com>
2024-01-09 23:44:39 -0800

    Fizz Buzz

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 for number in range(0, 100):
-    if number in range(0, 10):
-        print(f"0{number}, ", end="")
-    elif number == 99:
-        print(f"{number}")
+    if number == 99:
+        print("{}".format(number))
     else:
-        print(f"{number}, ", end="")
+        print("{:02}, ".format(number), end="")
