02caa9722dc2eea32cb814f661ad4f2abde1a688
Nancy <njerin016@gmail.com>
2024-01-20 10:10:20 -0800

    corrections

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-
 def no_c(my_string):
-    result = ""
-    for letter in my_string:
-        if (letter.lower() == "c" or letter.upper() == "C"):
-            continue
-        result += letter
-
-    print(result)
-
+    listofchars = list(my_string)
+    for char in listofchars:
+        if char == 'c' or char == 'C':
+            listofchars.remove(char)
+    return("".join(listofchars))
