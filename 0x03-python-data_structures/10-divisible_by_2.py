fea919084fb912ce8c4b5f839f322e17c346f95a
Nancy <njerin016@gmail.com>
2024-01-23 01:55:58 -0800

    C is tricky especially when combined with Python

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
 def divisible_by_2(my_list=[]):
+    new list = []
     for number in my_list:
         if number % 2 == 0:
-            print("{} is divisible by 2".format(number))
+            new_list.append(True)
         else:
-            print("{} is not divisible by 2".format(number))
+            new_list.append(False)
+
+    return new_list
