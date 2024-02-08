48256a572de18b6e5ad0d45a6c7aca1372636a36
Nancy Wachira <njerin016@gmail.com>
2024-02-08 00:17:02 -0800

    1 task failing so many checks

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,9 +1,6 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 def weight_average(my_list=[]):
-    if not my_list:
-        return (0)
-    else:
-        sum_multiplied_values = sum(score * weight for score, weight in my_list)
-        addition_of_weights = sum(weight for _, weight in my_list)
-        average_weight = (sum_multiplied_values / addition_of_weights)
-    return (average_weight)
+    if my_list:
+        return sum(a * b for a, b in my_list) / sum(b for a, b in my_list)
+    return 0
+    # = ((1 * 2) + (2 * 1) + (3 * 10) + (4 * 2)) / (2 + 1 + 10 + 2)
