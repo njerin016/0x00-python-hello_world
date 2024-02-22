9bb7c612de75e332ed74e751a9832cd80368f047
Nancy Wachira <njerin016@gmail.com>
2024-02-22 06:52:54 -0800

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+class Square:
+    """
+    This class defines a square
+    Attributes: the square has a size
+    Methods:(None)
+    It has no methods.
+    """
+    def __init__(self, size):
+        """
+        Initializing a new instance of the Square class
+        size of the square
+        """
+        self.__size = size
